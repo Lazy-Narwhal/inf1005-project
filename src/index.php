d8fa2f74e0a0602032cf09478af0c3df7e9834ff
Tristan <tristankohrh@gmail.com>
2025-11-13 14:01:05 +0800

    Update index.php

--- src/index.php
+++ src/index.php
@@ -2,24 +2,14 @@
 <html lang="en">
     <head>
         <title>Esacpe Quest</title>
-        <?php
-            include "inc/head.inc.php"
-        ?>
+        <?php include "inc/head.inc.php"?>
     </head>
     <body>
-        <?php
-            include "inc/nav.inc.php"
-        ?>
-        <?php
-            include "inc/header.inc.php"
-        ?>
+        <?php include "inc/nav.inc.php"?>
+        <?php include "inc/header.inc.php"?>
         <main>
             <!-- add ur code here -->
         </main>
-        <?php
-            include "inc/footer.inc.php"
-        ?>
-    
+        <?php include "inc/footer.inc.php"?
     </body>
-
 </html>
