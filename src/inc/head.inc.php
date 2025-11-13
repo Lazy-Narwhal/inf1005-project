16dbebb0dec2bbafe8537723f9cfbd8d724c550a
loghogjog <tantris.tan345@gmail.com>
2025-11-13 12:26:40 +0800

    Reapply "bookings page first half"
    This reverts commit 6ce9845dbc4fb18a1de61dd055c01ba7bdea9b75.
    
    what is this

--- src/inc/head.inc.php
+++ src/inc/head.inc.php
@@ -8,16 +8,7 @@
     integrity="sha384-4bw+/aepP/YC94hEpVNVgiZdgIC5+VKNBQNGCHeKRQN+PtmoHDEXuppvnDJzQIu9"
     crossorigin="anonymous">
 <link rel="stylesheet" href="css/style.css">
-<!--
-<style>
-    body {
-        background-image: url(images/surprise.jpg);
-        background-size: cover;
-        background-position: center 125px;
-        
-    }
-</style>
--->
+
 <!--Bootstrap JS-->
 <script defer
     src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.1/dist/js/bootstrap.bundle.min.js"
@@ -26,3 +17,4 @@
 </script>
 <!-- Custom JS -->
 <script defer src="js/main.js"></script>
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.7.1/jquery.min.js"></script>
